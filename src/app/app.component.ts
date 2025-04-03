d47e2ff3bc008b872aa769943e40b8841b9b635c
Gokul <iamgokulnathanmcs@gmail.com>
2025-04-03 18:55:42 +0530

    Changed Ui project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
   templateUrl: './app.component.html',
+  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'text from js';
+  title = 'azure-learning-Ui';
 }
